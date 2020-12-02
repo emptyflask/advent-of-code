07b55b47cc58820ab5a59f2f3619c4e6bfdae2d8
Jon Roberts <jon@emptyflask.net>
2020-12-02 00:02:47 -0600

    2020: Day 1

---
+++ 2020/01/solve.hs
@@ -0,0 +1,21 @@
+main :: IO ()
+main = do
+  input <- readFile "input.txt"
+  let list = read <$> lines input
+  print $ "Part 1: " <> (show $ solve1 list)
+  print $ "Part 2: " <> (show $ solve2 list)
+
+solve1 :: [Int] -> Maybe Int
+solve1 xs = mHead $ [ a*b | a <- xs,
+                            b <- xs,
+                            a + b == 2020 ]
+
+solve2 :: [Int] -> Maybe Int
+solve2 xs = mHead $ [ a*b*c | a <- xs,
+                              b <- xs,
+                              c <- xs,
+                              a + b + c == 2020 ]
+
+mHead :: [a] -> Maybe a
+mHead (x:_) = Just x
+mHead []    = Nothing
